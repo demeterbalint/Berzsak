72bf17b1be8196764d58df88b7e669828db97329
Demeter Bálint <demibalint@gmail.com>
2025-09-16 22:57:05 +0200

    project-page created

--- berzsak/src/app/app.routes.ts
+++ berzsak/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import {Routes} from '@angular/router';
 import {ProjectComponent} from './components/project/project.component';
+import {ProjectPageComponent} from './components/project-page/project-page.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: '/main', pathMatch: 'full'},
   {path: 'main', component: ProjectComponent},
+  {path: 'project-page', component: ProjectPageComponent}
 ];
