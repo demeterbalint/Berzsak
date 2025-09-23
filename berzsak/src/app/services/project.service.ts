b2468e3cb34dac333276e2367bce6ceca29fb927
Demeter Bálint <demibalint@gmail.com>
2025-09-23 12:37:54 +0200

    project-details' longDescription updated to string array to handle grid appearance

--- berzsak/src/app/services/project.service.ts
+++ berzsak/src/app/services/project.service.ts
@@ -11,11 +11,8 @@ export class ProjectService {
       name: 'qt tea set system',
       slug: 'qt-tea-set-system',
       shortDescription: 'The tea set reimagines herbal tea for the modern, health-conscious generation, transforming a traditional ritual into a refined, intuitive, and contemporary experience.',
-      longDescription: 'Herbal tea doesn’t have to feel old-fashioned. Regular consumption of herbs is one of the simplest ways to support the harmony of body and mind — so why not make it exciting, fresh, and easy to enjoy every day? This project set out to reimagine herbal tea for a younger, health-conscious generation.\n' +
-        '\n' +
-        'The QT tea set is a contemporary system that brings new life to the ritual of tea drinking. It respects the cultural roots of herbs while turning them into a modern, intuitive experience that fits seamlessly into today’s fast-paced lifestyle. Beyond home use, the design also aims to elevate the way tea is served and enjoyed in hotels, restaurants, and other hospitality spaces, making the act of drinking tea more engaging and memorable. \n' +
-        '\n' +
-        'With its clean, minimal form, QT transforms tea preparation into a small ritual of pleasure and presence — turning herbal tea from a rare indulgence into a mindful moment, an inspiring part of everyday life.',
+      longDescription: ['Herbal tea doesn’t have to feel old-fashioned. Regular consumption of herbs is one of the simplest ways to support the harmony of body and mind — so why not make it exciting, fresh, and easy to enjoy every day? This project set out to reimagine herbal tea for a younger, health-conscious generation. The QT tea set is a contemporary system that brings new life to the ritual of tea drinking. It respects the cultural roots of herbs while turning them into a modern, intuitive experience that fits seamlessly into today’s fast-paced lifestyle.',
+      'Beyond home use, the design also aims to elevate the way tea is served and enjoyed in hotels, restaurants, and other hospitality spaces, making the act of drinking tea more engaging and memorable. With its clean, minimal form, QT transforms tea preparation into a small ritual of pleasure and presence — turning herbal tea from a rare indulgence into a mindful moment, an inspiring part of everyday life.'],
       projectData: 'Year: 2025'
                 + '<br>'
                 + 'Photo: Anna Hardy, Balázs Harmathy',
@@ -31,7 +28,8 @@ export class ProjectService {
       name: 'demeter mixer grinder',
       slug: 'demeter-mixer-grinder',
       shortDescription: 'Meet Demeter 2310 — the all-in-one blender and scale that brings style, simplicity, and precision to your kitchen. No more confusing buttons, messy counters, or oversized jars — just effortless smoothies and meals, tailored to you.',
-      longDescription: 'Let us paint you a picture. It’s Friday afternoon, and you want to make a smoothie, but you are fed up with not knowing which button on your blender does what. You are tired of the mess you keep making in the kitchen with all the dishes, scales, and ingredients. Your blender jar is too big for one serving, so you decide sadly in the end not to make a smoothie at all. That does sound pretty bad, right? All your struggles come to an end with our revolutionary product. Introducing Demeter 2310, the solution to your problems, your companion in the kitchen, now no more confusing knobs, the scale and the blender are two in one. Choose the jar size that fits the purpose. Style, elegance, convenience, Demeter.',
+      longDescription: ['Let us paint you a picture. It’s Friday afternoon, and you want to make a smoothie, but you are fed up with not knowing which button on your blender does what. You are tired of the mess you keep making in the kitchen with all the dishes, scales, and ingredients. Your blender jar is too big for one serving, so you decide sadly in the end not to make a smoothie at all.',
+        'That does sound pretty bad, right? All your struggles come to an end with our revolutionary product. Introducing Demeter 2310, the solution to your problems, your companion in the kitchen, now no more confusing knobs, the scale and the blender are two in one. Choose the jar size that fits the purpose. Style, elegance, convenience, Demeter.'],
       projectData: 'Year: 2024'
         + '<br>'
         + 'Co-designer: Máté Guthy',
@@ -46,11 +44,8 @@ export class ProjectService {
       name: 'archproject shoe insole system',
       slug: 'archproject-shoe-insole-system',
       shortDescription: 'The Archproject insole kit brings innovation to every step — combining smart sensors and adaptive design to evolve with your feet, turning orthopedic care into a sleek, empowering path to comfort, posture, and long-term well-being.',
-      longDescription: 'Foot health is often overlooked — until pain starts getting in the way of everyday life. Sedentary lifestyles, hard urban surfaces, and unsupportive shoes have made flat feet and posture problems more common than ever, and when left untreated they can lead to serious complications.\n' +
-        '\n' +
-        'Archproject reimagines orthopedic care as something smart, sleek, and stigma-free. This adaptive insole kit uses pressure sensors, IoT technology, and a shape-shifting design to keep up with the natural changes in your feet — ensuring lasting comfort and proper support over time.\n' +
-        '\n' +
-        'With its refined, modular design, Archproject turns foot health into a simple, empowering, and even engaging part of daily life — promoting better posture, comfort, and long-term well-being.',
+      longDescription: ['Foot health is often overlooked — until pain starts getting in the way of everyday life. Sedentary lifestyles, hard urban surfaces, and unsupportive shoes have made flat feet and posture problems more common than ever, and when left untreated they can lead to serious complications. Archproject reimagines orthopedic care as something smart, sleek, and stigma-free.',
+      'This adaptive insole kit uses pressure sensors, IoT technology, and a shape-shifting design to keep up with the natural changes in your feet — ensuring lasting comfort and proper support over time. With its refined, modular design, Archproject turns foot health into a simple, empowering, and even engaging part of daily life — promoting better posture, comfort, and long-term well-being.'],
       projectData: 'Year: 2023\n',
       mainImgUrl: ['/archproject-shoe-insole-system/5846/archproject_5846.webp', '/archproject-shoe-insole-system/2400/archproject_2400.webp', '/archproject-shoe-insole-system/1800/archproject_1800.webp', '/archproject-shoe-insole-system/1200/archproject_1200.webp', '/archproject-shoe-insole-system/600/archproject_600.webp', '/archproject-shoe-insole-system/300/archproject_300.webp'],
       imageUrls: [['/archproject-shoe-insole-system/5846/archproject_5846.webp', '/archproject-shoe-insole-system/2400/archproject_2400.webp', '/archproject-shoe-insole-system/1800/archproject_1800.webp', '/archproject-shoe-insole-system/1200/archproject_1200.webp', '/archproject-shoe-insole-system/600/archproject_600.webp', '/archproject-shoe-insole-system/300/archproject_300.webp'],
@@ -62,11 +57,8 @@ export class ProjectService {
       name: 'depresszó coffee glass set',
       slug: 'depresszó-coffee-glass-set',
       shortDescription: 'The dePresszó coffee set transforms Hungarian sarcasm and pessimism into design — a playful blend of tradition, humor and function where the glasses, like the outlook, are always half empty — or half full…',
-      longDescription: 'Coffee is more than just a drink — it’s a cultural statement. Many nations have their own coffee specialties, from Café Cubano to Irish Coffee, each reflecting tradition and even national stereotypes. In Hungary, coffee arrived during the Turkish occupation, first tied to the ominous phrase “feketeleves” (“the black soup”). Over time, it became a symbol of cultural and intellectual life, with legendary cafés like the New York Café shaping history.\n' +
-        '\n' +
-        'This project reimagines that story through a fictional Hungarian coffee: the dePresszó — a darkly humorous mix of espresso and Unicum that playfully embraces Hungarian pessimism. Its name nods to the classic question: “Half full or half empty?” — which, for Hungarians, is naturally half empty.\n' +
-        '\n' +
-        'The resulting 12-piece glass coffee set embodies this idea: one glass in each pair always stands upside down, so the set can never be completely full. Minimal yet expressive, it blends humor, functionality, and cultural commentary in a single design.',
+      longDescription: ['Coffee is more than just a drink — it’s a cultural statement. Many nations have their own coffee specialties, from Café Cubano to Irish Coffee, each reflecting tradition and even national stereotypes. In Hungary, coffee arrived during the Turkish occupation, first tied to the ominous phrase “feketeleves” (“the black soup”). Over time, it became a symbol of cultural and intellectual life, with legendary cafés like the New York Café shaping history.',
+        'This project reimagines that story through a fictional Hungarian coffee: the dePresszó — a darkly humorous mix of espresso and Unicum that playfully embraces Hungarian pessimism. Its name nods to the classic question: “Half full or half empty?” — which, for Hungarians, is naturally half empty. The resulting 12-piece glass coffee set embodies this idea: one glass in each pair always stands upside down, so the set can never be completely full. Minimal yet expressive, it blends humor, functionality, and cultural commentary in a single design.'],
       projectData: 'Year: 2024'
         + '<br>'
         + 'Photo: Barna Berzsák, Milán Rácmolnár',
@@ -79,11 +71,8 @@ export class ProjectService {
       name: 'resq avalanche transmitter',
       slug: 'resq-avalanche-transmitter',
       shortDescription: 'ResQ makes avalanche rescue fast, intuitive, and optimal — turning chaos into confident action when every second counts.',
-      longDescription: 'Avalanches happen fast — and survival depends on quick, calm action. For freeriders, the best chance of rescue is not waiting for professionals but acting within their own group — yet panic, poor visibility, and confusing equipment can cost precious minutes.\n' +
-        '\n' +
-        'ResQ was designed to change that. This smart avalanche transmitter turns life-saving gear into something intuitive, stylish, and rider-friendly. Its compass-like form and 360° LED ring guide rescuers toward victims with clear visual feedback, while GPS alerts, fall detection, and built-in tutorials keep users prepared and informed in every situation.\n' +
-        '\n' +
-        'With its sleek iris-lock strap system, ResQ stays secure but always within reach, making it easy to grab and use when every second counts. By combining technology, ergonomics, and freerider style, ResQ transforms avalanche rescue from panic into confident, coordinated action.',
+      longDescription: ['Avalanches happen fast — and survival depends on quick, calm action. For freeriders, the best chance of rescue is not waiting for professionals but acting within their own group — yet panic, poor visibility, and confusing equipment can cost precious minutes. ResQ was designed to change that. This smart avalanche transmitter turns life-saving gear into something intuitive, stylish, and rider-friendly.',
+        'Its compass-like form and 360° LED ring guide rescuers toward victims with clear visual feedback, while GPS alerts, fall detection, and built-in tutorials keep users prepared and informed in every situation. With its sleek iris-lock strap system, ResQ stays secure but always within reach, making it easy to grab and use when every second counts. By combining technology, ergonomics, and freerider style, ResQ transforms avalanche rescue from panic into confident, coordinated action.'],
       projectData: 'Year: 2024\n',
       mainImgUrl: ['/resq-avalanche-transmitter/5846/resq_5846.webp', '/resq-avalanche-transmitter/2400/resq_2400.webp', '/resq-avalanche-transmitter/1800/resq_1800.webp', '/resq-avalanche-transmitter/1200/resq_1200.webp', '/resq-avalanche-transmitter/600/resq_600.webp', '/resq-avalanche-transmitter/300/resq_300.webp'],
       imageUrls: [['/resq-avalanche-transmitter/5846/resq_5846.webp', '/resq-avalanche-transmitter/2400/resq_2400.webp', '/resq-avalanche-transmitter/1800/resq_1800.webp', '/resq-avalanche-transmitter/1200/resq_1200.webp', '/resq-avalanche-transmitter/600/resq_600.webp', '/resq-avalanche-transmitter/300/resq_300.webp'],
@@ -97,7 +86,7 @@ export class ProjectService {
       name: 'chess',
       slug: 'chess',
       shortDescription: 'This experimental chess set explores how form can influence play. Inspired by Victor Vasarely and op-art, the chessboard alters perception — subtly shaping players’ decisions and the game’s dynamics.',
-      longDescription: 'his experimental chess set explores how form can influence play. Inspired by Victor Vasarely and op-art, the chessboard alters perception — subtly shaping players’ decisions and the game’s dynamics.',
+      longDescription: ['This experimental chess set explores how form can influence play. Inspired by Victor Vasarely and op-art, the chessboard alters perception — subtly shaping players’ decisions and the game’s dynamics.'],
       projectData: 'Year: 2021'
         + '<br>'
         + 'Photo: Réka Nyikos',
@@ -109,7 +98,7 @@ export class ProjectService {
       name: 'flying shark',
       slug: 'flying-shark',
       shortDescription: 'Red Bull Flugtag challenges teams to design, build, and launch flying machines over water. Inspired by the hit series Arcane, our creation soared — carrying the pilot an impressive 15 meters through the air.',
-      longDescription: 'Red Bull Flugtag challenges teams to design, build, and launch flying machines over water. Inspired by the hit series Arcane, our creation soared — carrying the pilot an impressive 15 meters through the air.',
+      longDescription: ['Red Bull Flugtag challenges teams to design, build, and launch flying machines over water. Inspired by the hit series Arcane, our creation soared — carrying the pilot an impressive 15 meters through the air.'],
       projectData: 'Year: 2022'
         + '<br>'
         + 'Co-designers: Virág Oszkai, Róbert Kristóffy, Mátyás Galavits'
